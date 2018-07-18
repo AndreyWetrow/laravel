d2e6f8fefc31c2ddd750bd6f1c87b6400a7004f8
wetrow <andrnazkem@mail.ru>
2018-07-18 12:22:01 +0700

    change welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Учим Laravel
                 </div>
 
                 <div class="links">
